2553d74a4419891d34bcfcf91eaa4230ab903de8
Robert Günzler <r@gnzler.io>
2020-01-23 12:28:42 +0100

    wip: shellcheck fixes

--- cmd/a2o-migrate/test/integration.sh
+++ cmd/a2o-migrate/test/integration.sh
@@ -2,7 +2,7 @@
 
 RT=${RT:-balena-engine}
 CONTAINERIZED=${CONTAINERIZED:-}
-PROJECT="$(dirname $(readlink -f $0))/.."
+PROJECT="$(dirname "$(readlink -f "${0}")")/.."
 IMAGE=${IMAGE:-balena/balena-engine:beind}
 
 container_name="balena_a2o_integration_test"
@@ -10,16 +10,16 @@ balena_container_flags="--rm --detach --name ${container_name} --privileged -v v
 
 set -ex
 
-[ -n "$CONTAINERIZED" ] && {
-    exec ${PROJECT}/test/integration-test.sh
+[ -n "${CONTAINERIZED}" ] && {
+    exec "${PROJECT}/test/integration-test.sh"
 }
 
 test_out_dir=$(mktemp -d /tmp/a2o-migrate_test_XXXX)
-trap "{ rm -rf ${test_out_dir}; }" EXIT
+trap '{ rm -rf ${test_out_dir}; }' EXIT
 
 # start balenaEngine with aufs
-$RT run $balena_container_flags $IMAGE --debug --storage-driver=aufs
-trap "{ ${RT} stop -t 3 ${container_name}; ${RT} volume rm -f varlibbalena; }" EXIT
+$RT run "${balena_container_flags}" "${IMAGE}" --debug --storage-driver=aufs
+trap '{ ${RT} stop -t 3 "${container_name}"; ${RT} volume rm -f varlibbalena; }' EXIT
 
 sleep 1
 $RT exec ${container_name} balena-engine info || exit 1
@@ -31,23 +31,23 @@ RUN rm -R /tmp/d1 && mkdir /tmp/d1 && touch /tmp/d1/d1f2 && rm /tmp/f1
 RUN ln -s /tmp/d1/d1f2 /tmp/flnk
 EOF
 
-$RT exec ${container_name} balena-engine run --name a2o-test-container a2o-test ls -lR /tmp > ${test_out_dir}/stdout_before
+$RT exec "${container_name}" balena-engine run --name a2o-test-container a2o-test ls -lR /tmp > "${test_out_dir}/stdout_before"
 
 # run migration
-$RT exec -e CONTAINERIZED=1 ${container_name} /src/test/$(basename $0)
+$RT exec -e CONTAINERIZED=1 "${container_name}" /src/test/"$(basename "${0}")"
 
 # stop aufs daemon
-$RT stop -t 3 ${container_name}
+$RT stop -t 3 "${container_name}"
 
 # start balenaEngine with overlay2
-$RT run $balena_container_flags $IMAGE --debug --storage-driver=overlay2
+$RT run "${balena_container_flags}" "${IMAGE}" --debug --storage-driver=overlay2
 sleep 1
-$RT inspect ${container_name} &>/dev/null || exit 1
+$RT inspect "${container_name}" 1>&2 2>/dev/null || exit 1
 
 # check if we still are able to create a container from the a2o-test image
-$RT exec ${container_name} balena-engine run --rm a2o-test ls -lR /tmp > ${test_out_dir}/stdout_after
+$RT exec "${container_name}" balena-engine run --rm a2o-test ls -lR /tmp > "${test_out_dir}/stdout_after"
 # check if rewriting the container storage drivers worked
-$RT exec ${container_name} balena-engine start a2o-test-container
+$RT exec "${container_name}" balena-engine start a2o-test-container
 
 # check ls -lR /tmp output
-diff ${test_out_dir}/stdout_before ${test_out_dir}/stdout_after || true
+diff "${test_out_dir}/stdout_before" "${test_out_dir}/stdout_after" || true
